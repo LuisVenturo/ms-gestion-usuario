19bce3597f3423934e70ca721d97fe6be0540ff6
Luis Venturo <venturomoralesluis@gmail.com>
2024-09-17 23:29:55 -0500

    Desarrollo de la aplicación

---
+++ src/test/java/com/codigo/ms_gestion_usuario/MsGestionUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.ms_gestion_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
